a2f57c85ab62568a4e5190c41e252906fe041c7d
Viet Anh Vo <vietanhvo@pm.me>
2025-05-21 11:11:26 +0700

    Add node n and add @minimaltech/eslint-node

--- packages/node/index.ts
+++ packages/node/index.ts
@@ -1,6 +1,7 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import commonPlugin from "@minimaltech/eslint-common";
+import nodePlugin from "eslint-plugin-n";
 
 const compat = new FlatCompat({
   recommendedConfig: js.configs.recommended,
@@ -11,7 +12,12 @@ const configs = [
   ...compat.extends("@loopback/eslint-config"),
   ...commonPlugin,
   {
+    plugins: {
+      n: nodePlugin,
+    },
     rules: {
+      "n/prefer-node-protocol": ["error"],
+
       // OFF
       "@typescript-eslint/no-inferrable-types": "off",
       "@typescript-eslint/no-misused-promises": "off",
