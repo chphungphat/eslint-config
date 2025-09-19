ae8b5b76e5e7ab23c9f1c458572ef26e95dfb6f4
Phat Nguyen <tanphat199@gmail.com>
2025-09-19 17:37:16 +0700

    fix

--- packages/node/lb-eslint.ts
+++ packages/node/lb-eslint.ts
@@ -4,110 +4,56 @@ import tsEslint from "typescript-eslint";
 const lbRules = defineConfig(tsEslint.configs.recommended, {
   rules: {
     "prefer-const": "error",
+    "no-unused-labels": "error",
+    "no-new-wrappers": "error",
+    "no-throw-literal": "error",
+    "no-unused-expressions": "error",
+    "no-var": "error",
+    eqeqeq: ["error", "smart"],
+    "no-void": "error",
+
+    "no-useless-escape": "warn",
+
     "no-mixed-operators": "off",
     "no-console": "off",
-    // 'no-undef': 'off',
     "no-inner-declarations": "off",
-    // TypeScript allows method overloading
     "no-dupe-class-members": "off",
-    "no-useless-escape": "off",
-    // TypeScript allows the same name for namespace and function
     "no-redeclare": "off",
+    "no-caller": "error",
+    "no-invalid-this": "off",
+    "no-shadow": "error",
+    "no-return-await": "off",
+    camelcase: "off",
 
-    /**
-     * Rules imported from eslint-config-loopback
-     */
     // "mocha/handle-done-callback": "error",
     // "mocha/no-exclusive-tests": "error",
     // "mocha/no-identical-title": "error",
     // "mocha/no-nested-tests": "error",
     // "no-array-constructor": "error",
 
-    /**
-     * TypeScript specific rules
-     * See https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#supported-rules
-     */
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/indent": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/no-object-literal-type-assertion": "off",
-    "@typescript-eslint/no-parameter-properties": "off",
-    "@typescript-eslint/no-angle-bracket-type-assertion": "off",
-    "@typescript-eslint/prefer-interface": "off",
-    "@typescript-eslint/no-namespace": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/no-triple-slash-reference": "off",
-    "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
-
-    // Disable warning  Missing return type on function for now
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-
-    /**
-     * The following rules are enforced to support legacy tslint configuration
-     */
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/ROADMAP.md
-    // Rules mapped from `@loopback/tslint-config/tslint.common.json
-    "@typescript-eslint/adjacent-overload-signatures": "error", // tslint:adjacent-overload-signatures
-    "@typescript-eslint/prefer-for-of": "error", // tslint:prefer-for-of
-    "@typescript-eslint/unified-signatures": "error", // tslint:unified-signatures
-    "@typescript-eslint/no-explicit-any": "error", // tslint:no-any
-
-    "no-unused-labels": "error", // tslint:label-position
-    "no-caller": "error", // tslint:no-arg
-    "no-new-wrappers": "error", // tslint:no-construct
-    // 'no-redeclare': 'error', // tslint:no-duplicate-variable
-
-    "no-invalid-this": "off",
+    "@typescript-eslint/adjacent-overload-signatures": "error",
+    "@typescript-eslint/prefer-for-of": "error",
+    "@typescript-eslint/unified-signatures": "error",
+    "@typescript-eslint/no-explicit-any": "error",
     "@typescript-eslint/no-invalid-this": ["error"],
     "@typescript-eslint/no-misused-new": "error",
-
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-shadow.md#how-to-use
-    "no-shadow": "off",
+    "@typescript-eslint/no-use-before-define": "error",
     "@typescript-eslint/no-shadow": "error",
-
-    "no-throw-literal": "error", // tslint:no-string-throw
-
     "@typescript-eslint/no-unused-vars": [
       "error",
-      {
-        vars: "all",
-        args: "none", // none - do not check arguments
-        ignoreRestSiblings: false,
-      },
-    ], // tslint:no-unused-variable
-    "no-unused-expressions": "error", // tslint:no-unused-expression
-    "no-var": "error", // tslint:no-var-keyword
-    eqeqeq: ["error", "smart"], // tslint:triple-equals: [true, 'allow-null-check', 'allow-undefined-check'],
-
-    // Rules mapped from `@loopback/tslint-config/tslint.build.json
-    "@typescript-eslint/await-thenable": "error", // tslint:await-promise: [true, 'PromiseLike', 'RequestPromise'],
+      { vars: "all", args: "none", ignoreRestSiblings: false },
+    ],
+    "@typescript-eslint/await-thenable": "error",
     "@typescript-eslint/no-floating-promises": "error",
-
-    "no-void": "error", // tslint:no-void-expression: [true, 'ignore-arrow-function-shorthand'],
-
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/consistent-type-assertions": "off",
     "@typescript-eslint/no-misused-promises": "error",
-
     "@typescript-eslint/prefer-optional-chain": "error",
     "@typescript-eslint/prefer-nullish-coalescing": [
       "error",
-      { allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing: true }, // See https://github.com/typescript-eslint/typescript-eslint/pull/6174
+      { allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing: true },
     ],
-    "@typescript-eslint/no-extra-non-null-assertion": "error",
 
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/return-await.md#how-to-use
     "@typescript-eslint/return-await": "error",
-    // note we must disable the base rule as it can report incorrect errors
-    "no-return-await": "off",
-
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md
-    camelcase: "off",
     "@typescript-eslint/naming-convention": [
       "error",
       {
@@ -208,6 +154,25 @@ const lbRules = defineConfig(tsEslint.configs.recommended, {
         filter: "^_$",
       },
     ],
+
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-object-literal-type-assertion": "off",
+    "@typescript-eslint/no-parameter-properties": "off",
+    "@typescript-eslint/no-angle-bracket-type-assertion": "off",
+    "@typescript-eslint/prefer-interface": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-triple-slash-reference": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-require-imports": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/consistent-type-assertions": "off",
   },
 });
 
